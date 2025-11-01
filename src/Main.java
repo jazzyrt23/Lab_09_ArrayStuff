fba34b02e191580e927a4369f3567e09d74e3852
Jasmine Thomas <thoma9jr@mail.uc.edu>
2025-11-01 11:22:01 -0400

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,98 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Part 1
+        // Task 1
+        int[] dataPoints = new int[100];
+
+        // Task 2 - fill array with random numbers 1-100
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rand.nextInt(100) + 1;
         }
+
+        // Task 3 - print all numbers separated by |
+        System.out.println("Data Points:");
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (i < dataPoints.length - 1) {
+                System.out.print(dataPoints[i] + " | ");
+            } else {
+                System.out.print(dataPoints[i]);
+            }
+        }
+        System.out.println(); // new line
+
+        // Task 4 - sum and average
+        int sum = 0;
+        for (int i = 0; i < dataPoints.length; i++) {
+            sum += dataPoints[i];
+        }
+        double average = (double) sum / dataPoints.length;
+
+        System.out.println("The sum of dataPoints is: " + sum);
+        System.out.println("The average of dataPoints is: " + average);
+
+        // Part 2
+        // Task 5 - get number from user
+        int userValue = SafeInput.getRangedInt(in, "Enter a number between 1 and 100", 1, 100);
+
+        // Task 6 - count how many times user value appears
+        int count = 0;
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == userValue) {
+                count++;
+            }
+        }
+        System.out.println("Your number " + userValue + " was found " + count + " times in the array.");
+
+        // Task 7 - find first occurrence of another user value
+        int searchValue = SafeInput.getRangedInt(in, "Enter another number between 1 and 100 to search for", 1, 100);
+        int foundIndex = -1;
+
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == searchValue) {
+                foundIndex = i;
+                break;
+            }
+        }
+
+        if (foundIndex != -1) {
+            System.out.println("The value " + searchValue + " was found at index " + foundIndex + ".");
+        } else {
+            System.out.println("The value " + searchValue + " was not found in the array.");
+        }
+
+        // Task 8 - find min and max
+        int min = dataPoints[0];
+        int max = dataPoints[0];
+
+        for (int i = 1; i < dataPoints.length; i++) {
+            if (dataPoints[i] < min) {
+                min = dataPoints[i];
+            }
+            if (dataPoints[i] > max) {
+                max = dataPoints[i];
+            }
+        }
+
+        System.out.println("The smallest number is: " + min);
+        System.out.println("The biggest number is: " + max);
+
+        // Task 9 - call method to get average
+        System.out.println("Average of dataPoints (using method): " + getAverage(dataPoints));
+    }
+
+    // Task 9 method
+    public static double getAverage(int[] values) {
+        int sum = 0;
+        for (int i = 0; i < values.length; i++) {
+            sum += values[i];
+        }
+        double avg = (double) sum / values.length;
+        return avg;
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class SafeInput {
+    public static int getRangedInt(Scanner in, String prompt, int low, int high) {
+        int value = 0;
+        boolean done = false;
+
+        do {
+            System.out.print(prompt + " [" + low + " - " + high + "]: ");
+            if (in.hasNextInt()) {
+                value = in.nextInt();
+                if (value >= low && value <= high) {
+                    done = true;
+                } else {
+                    System.out.println("Error: number must be between " + low + " and " + high);
+                }
+            } else {
+                System.out.println("Error: please enter an integer.");
+                in.next(); // clear bad input
+            }
+        } while (!done);
+
+        return value;
+    }
 }
